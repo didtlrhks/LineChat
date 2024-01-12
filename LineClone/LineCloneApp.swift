c23a354240f0088ce2074bc94ffb415a99f19792
sikwan <yysskk99999@naver.com>
2024-01-12 15:40:54 +0900

    init

---
+++ LineClone/LineCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineCloneApp.swift
+//  LineClone
+//
+//  Created by 양시관 on 1/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LineCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
