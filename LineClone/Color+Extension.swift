528caf551cadf4af093e094d62713e46c3b68617
sikwan <yysskk99999@naver.com>
2024-01-12 19:45:11 +0900

    asset setting

---
+++ LineClone/Color+Extension.swift
@@ -0,0 +1,30 @@
+//
+//  Extension.swift
+//  LineClone
+//
+//  Created by 양시관 on 1/12/24.
+//
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    static let bgh: Color = .init("bg-wh")
+    static let bk2ix: Color = .init("bk-2-fix")
+    static let bkext: Color = .init("bk-text")
+    static let blackix: Color = .init("black-fix")
+    static let chatg: Color = .init("chat-bg")
+    static let chatolorMe: Color = .init("chat-color-me")
+    static let chatolorOhter: Color = .init("chat-color-ohter")
+    static let chatotice: Color = .init("chat-notice")
+    static let coralg: Color = .init("coral_bg")
+    static let greyool: Color = .init("grey-cool")
+    static let greyeep: Color = .init("grey-deep")
+    static let greyix: Color = .init("grey-fix")
+    static let greyightVer2: Color = .init("grey-light-ver2")
+    static let greyight: Color = .init("grey-light")
+    static let lineAppColor: Color = .init("line-app-color")
+    static let mintg: Color = .init("mint_bg")
+    static let skyg: Color = .init("sky_bg")
+    static let whiteix: Color = .init("white-fix")
+}
