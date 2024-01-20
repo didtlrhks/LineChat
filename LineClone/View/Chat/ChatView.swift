d08acd473b4cae649f5dad451adcfab82c4748f5
sikwan <yysskk99999@naver.com>
2024-01-20 15:49:04 +0900

    chatView

--- LineClone/View/Chat/ChatView.swift
+++ LineClone/View/Chat/ChatView.swift
@@ -8,13 +8,50 @@
 import SwiftUI
 
 struct ChatView: View {
-    @StateObject var viewModel : ChatViewModel
-    
+    @StateObject var viewModel: ChatViewModel
+  //  @FocusState private var isFocused: Bool
+    @EnvironmentObject var navigationRouter : NavigationRouter
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+            ScrollView {
+              contentView
+            }
+        .background(Color.chatg)
+        .navigationBarBackButtonHidden()
+        .toolbar(.hidden, for: .tabBar)
+        .toolbar {
+            ToolbarItemGroup(placement: .navigationBarLeading) {
+                Button {
+                    navigationRouter.pop()
+                } label: {
+                    Image("back")
+                }
+                
+                Text(viewModel.otherUser?.name ?? "대화방이름")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(.bkext)
+            }
+            
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                Image(decorative: "search_chat")
+                Image(decorative: "bookmark")
+                Image(decorative: "settings")
+            }
+        }
+    }
+    
+    var contentView : some View {
+        EmptyView()
     }
 }
 
-#Preview {
-    ChatView(viewModel: .init(container: DIContainer(services: StubService()), chatRoomId: "chatRoom1_id", myUserId: "user1_id", otherUserId: "user2_id"))
+struct ChatView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStack {
+            ChatView(viewModel: .init(container: DIContainer(services: StubService()),
+                                      chatRoomId: "chatRoom1_id",
+                                      myUserId: "user1_id",
+                                      otherUserId: "user2_id"))
+        }
+    }
 }
