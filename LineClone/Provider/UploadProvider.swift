e5b1ff1445b5ad822e46b40b35f53eab28b1423b
sikwan <yysskk99999@naver.com>
2024-01-22 23:03:46 +0900

    upload Service add

--- LineClone/Provider/UploadProvider.swift
+++ LineClone/Provider/UploadProvider.swift
@@ -6,14 +6,33 @@
 //
 
 import Foundation
+import Combine
 import FirebaseStorage
+import FirebaseStorageCombineSwift
+
+enum UploadError: Error {
+    case error(Error)
+}
 
 protocol UploadProviderType {
-    func upload(path: String,data : Data, fileName: String) async throws -> URL
+    func upload(path: String, data: Data, fileName: String) -> AnyPublisher<URL, UploadError>
+    func upload(path: String, data: Data, fileName: String) async throws -> URL
 }
 
 class UploadProvider: UploadProviderType {
+    
     let storageRef = Storage.storage().reference()
+    
+    func upload(path: String, data: Data, fileName: String) -> AnyPublisher<URL, UploadError> {
+        let ref = storageRef.child(path).child(fileName)
+        
+        return ref.putData(data)
+            .flatMap { _ in
+                ref.downloadURL()
+            }
+            .mapError { .error($0) }
+            .eraseToAnyPublisher()
+    }
     
     func upload(path: String, data: Data, fileName: String) async throws -> URL {
         let ref = storageRef.child(path).child(fileName)
