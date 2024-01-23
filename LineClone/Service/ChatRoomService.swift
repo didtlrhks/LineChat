fbda4290dda61681fd169d257c1a3dfd46961d0a
sikwan <yysskk99999@naver.com>
2024-01-23 18:34:39 +0900

    chatroomService

--- LineClone/Service/ChatRoomService.swift
+++ LineClone/Service/ChatRoomService.swift
@@ -11,7 +11,7 @@ import Combine
 protocol ChatRoomServiceType {
     func createChatRoomIfNeeded(myUserId: String,otherUserId: String,otherUserName : String) -> AnyPublisher<ChatRoom,ServiceError>
     func loadChatRooms(myUserId: String) -> AnyPublisher<[ChatRoom],ServiceError>
-    
+    func updateChatRoomLastMessage(chatRoomId: String, myUserId: String, myUserName: String, otherUserId: String, lastMessage: String) -> AnyPublisher<Void, ServiceError>
 }
 
 class ChatRoomService : ChatRoomServiceType {
@@ -50,6 +50,12 @@ class ChatRoomService : ChatRoomServiceType {
         
     }
     
+    func updateChatRoomLastMessage(chatRoomId: String, myUserId: String, myUserName: String, otherUserId: String, lastMessage: String) -> AnyPublisher<Void, ServiceError> {
+         dbRepository.updateChatRoomLastMessage(chatRoomId: chatRoomId, myUserId: myUserId, myUserName: myUserName, otherUserId: otherUserId, lastMessage: lastMessage)
+            .mapError{ServiceError.error($0)}
+            .eraseToAnyPublisher()
+    }
+    
 }
 
 class StubChatRoomService : ChatRoomServiceType {
@@ -60,4 +66,11 @@ class StubChatRoomService : ChatRoomServiceType {
     func loadChatRooms(myUserId: String) -> AnyPublisher<[ChatRoom],ServiceError>{
         Just([.stub1, .stub2]).setFailureType(to: ServiceError.self).eraseToAnyPublisher()
     }
+    
+    func updateChatRoomLastMessage(chatRoomId: String, myUserId: String, myUserName: String, otherUserId: String, lastMessage: String) -> AnyPublisher<Void, ServiceError>{
+        
+            Empty().eraseToAnyPublisher()
+        
+    }
+    
 }
