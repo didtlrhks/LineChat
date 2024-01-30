9fab920eb01baac54e131329a318d7e94378bec4
sikwan <yysskk99999@naver.com>
2024-01-30 17:11:22 +0900

    ChatRoomObject

--- LineClone/Repository/DTO/ChatRoomObject.swift
+++ LineClone/Repository/DTO/ChatRoomObject.swift
@@ -15,7 +15,7 @@ struct ChatRoomObject: Codable {
     var otherUserName: String
     var otherUseId: String
 }
-
+// 여기에서도 마찬가지로 ChatRoomObject 구조체를 코더블 프로토콜을 따르게 설정해서 밑에 내용들이 json 직렬화(인코딩 ) 역직렬화 (디코딩) 등의 작업을 해줄수있게 해주는 역할을 함
 extension ChatRoomObject {
     func toModel() -> ChatRoom {
         .init(chatRoomId: chatRoomId,
@@ -23,7 +23,7 @@ extension ChatRoomObject {
               otherUserName: otherUserName,
               otherUseId: otherUseId)
     }
-}
+}//여기에서는 ChatRoomobject를 ChatRoom으로 보내주는 역할을 함
 
 extension ChatRoomObject {
     static var stub1: ChatRoomObject {
@@ -31,4 +31,4 @@ extension ChatRoomObject {
               otherUserName: "user2",
               otherUseId: "user2_id")
     }
-}
+}//이건 예상되는 내용의 값들을 넣어준것뿐임 
